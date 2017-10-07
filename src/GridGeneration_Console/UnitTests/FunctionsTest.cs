86142a99b5ba0dfcd0ca60dc6aa2292041d9d09f
sevoster <sevoster@gmail.com>
2017-10-07 17:20:37 +0300

    little refactoring

--- src/GridGeneration_Console/UnitTests/FunctionsTest.cs
+++ src/GridGeneration_Console/UnitTests/FunctionsTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
+using MeshRecovery_Lib;
 
 namespace UnitTests
 {
@@ -10,11 +11,11 @@ namespace UnitTests
         string PathToSources = @"tests/sources";
         string PathToAnswers = @"tests/answers";
 
-        string GraphNumerationToStaring(int[] grapgNumeration)
+        string GraphNumerationToString(int[] graphNumeration)
         {
-            string result = grapgNumeration[0].ToString();
-            for (int i = 0; i < grapgNumeration.Length; ++i)
-                result += " "+grapgNumeration.ToString();
+            string result = graphNumeration[0].ToString();
+            for (int i = 0; i < graphNumeration.Length; ++i)
+                result += " "+graphNumeration.ToString();
             return result;
         }
 
@@ -23,19 +24,19 @@ namespace UnitTests
         {
             string[] files = Directory.GetFiles(PathToSources);
             Assert.AreNotEqual(files.Length, 0);
-                        
+
             for (int i = 0; i < files.Length; ++i)
             {
                 string current_file_name = files[i].Remove(0, (PathToSources + "\\").Length);
                 
-                Graph.Loader.LoadGraphFromMETISFormat(files[i], out long[] xadj, out int[] adjncy);
-                bool valid = MeshRecovery_Lib.MeshRecovery.Validate(xadj, 0, adjncy, out int meshDimension);
-                int numerate = MeshRecovery_Lib.MeshRecovery.Numerate(xadj, 0, adjncy, out int[] graphNumeration);
+                Loader.LoadGraphFromMETISFormat(files[i], out long[] xadj, out int[] adjncy);
+                bool valid = MeshRecovery.Validate(xadj, 0, adjncy, out int meshDimension);
+                int numerate = MeshRecovery.Numerate(xadj, 0, adjncy, out int[] graphNumeration);
                 //TODO: Dinar: check possible values 
                 if (graphNumeration == null) graphNumeration = new int[] { 0, 0 };
                 StreamReader read = new StreamReader(PathToAnswers + "/" + current_file_name.Replace(".graph", ".num"));
                 //TODO: Dinar: resolve answers for cube and line graphss
-                Assert.AreEqual(GraphNumerationToStaring(graphNumeration), read.ReadToEnd());
+                Assert.AreEqual(GraphNumerationToString(graphNumeration), read.ReadToEnd());
                 read.Close();
             }
             
---
+++ src/GridGeneration_Lib/MeshRecovery.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeshRecovery_Lib
+{
+    public static class MeshRecovery
+    {
+        /// <summary>
+        /// Method checks that input graph corresponds to a regular grid
+        /// </summary>
+        /// <param name="xadj">Index array for graph</param>
+        /// <param name="size">Size of index array</param>
+        /// <param name="adjncy">Adjacency list</param>
+        /// <param name="meshDimension">Dimension of a regular grid that corresponds to the input graph</param>
+        /// <returns>true if graph corresponds to a regular grid, otherwise false</returns>
+        public static bool Validate(long[] xadj, int size, int[] adjncy, out int meshDimension)
+        {
+            // TODO: Implement
+            meshDimension = 0;
+            return false;
+        }
+
+        /// <summary>
+        /// Method restores geometry information for each graph node
+        /// </summary>
+        /// <param name="xadj">Index array for graph</param>
+        /// <param name="size">Size of index array</param>
+        /// <param name="adjncy">Adjacency list</param>
+        /// <param name="graphNumeration">Restored numeration for the input graph</param>
+        /// <returns>
+        /// Return code:
+        /// 0 - success
+        /// -1 - error
+        /// </returns>
+        public static int Numerate(long[] xadj, int size, int[] adjncy, out int[] graphNumeration)
+        {
+            // TODO: Implement
+            // TODO: graphNumeration may be 1-3 dimensional array
+            graphNumeration = null;
+            return 0;
+        }
+    }
+}
