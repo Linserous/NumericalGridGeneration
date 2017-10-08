06b1b6794d154beef81ad3e713b4697507ba99a5
MacBookAir\ShestovaA <ahsas078@gmail.com>
2017-10-08 23:19:36 +0300

    test for graph

---
+++ src/GridGeneration_Console/UnitTests/TestForGraph.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GridGeneration_Lib;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestForGraph
+    {
+        [TestMethod]
+        public bool TestMethod1()
+        {
+            long[] xadj = {0, 2, 4, 7, 10,12,15,18};
+            int[] adjncy = { 1, 3, 0, 2, 1, 3, 5, 0, 2, 4, 3, 5, 2, 4, 6, 1, 5 };
+            Graph graph = new Graph(xadj, adjncy);
+            graph.GetAdjVertices(2, out int[] buff);
+            
+            long count = graph.GetAdjVerticesCount(2);
+            bool result = (buff.Length == count) && (graph.GetVerticesCount() == 7 ) && (graph.GetEdgeCount() == 9);
+            return result;
+        }
+    }
+}
