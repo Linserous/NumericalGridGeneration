7dce18b29e9ceb6da860776f717b517d4023f461
eshulankina <eljogar@mail.ru>
2017-12-01 04:26:56 +0300

    Fixed bug with check in 3D Numerator

--- src/MeshRecovery_Lib/ThreeDimVertexNumerator.cs
+++ src/MeshRecovery_Lib/ThreeDimVertexNumerator.cs
@@ -90,7 +90,7 @@ namespace MeshRecovery_Lib
                             graphNumeration[vertices[0]][1] + offset[1],
                             graphNumeration[vertices[0]][2] + offset[2] };
 
-                        newIndexFound = !NumerationHelper.IndexExists(index, graphNumeration);
+                        newIndexFound = !NumerationHelper.IndexExists3(index, graphNumeration);
                         if (newIndexFound)
                         {
                             graphNumeration[vertex] = index;
@@ -171,60 +171,6 @@ namespace MeshRecovery_Lib
                     ++localDirection;
                 }
                 return alts.Count() == 0 ? null : alts;
-                //// Create 2 alternatives, for example:  [1, -1], [0, -2] from: [0, -1], [1, -2] 
-                //var alternative1 = new int[] { graphNumeration[vertices[0]][0], graphNumeration[vertices[1]][1], graphNumeration[vertices[0]][2] };
-                //var alternative2 = new int[] { graphNumeration[vertices[1]][0], graphNumeration[vertices[0]][1], graphNumeration[vertices[0]][2] };
-                //var alternative3 = new int[] { graphNumeration[vertices[0]][0], graphNumeration[vertices[0]][1], graphNumeration[vertices[1]][2] };
-                //var alternative4 = new int[] { graphNumeration[vertices[1]][0], graphNumeration[vertices[0]][1], graphNumeration[vertices[1]][2] };
-                //var alternative5 = new int[] { graphNumeration[vertices[0]][0], graphNumeration[vertices[1]][1], graphNumeration[vertices[1]][2] };
-                //var alternative6 = new int[] { graphNumeration[vertices[1]][0], graphNumeration[vertices[1]][1], graphNumeration[vertices[0]][2] };
-
-
-                //var alt1Exists = NumerationHelper.IndexExists3(alternative1, graphNumeration);
-                //var alt2Exists = NumerationHelper.IndexExists3(alternative2, graphNumeration);
-                //var alt3Exists = NumerationHelper.IndexExists3(alternative3, graphNumeration);
-                //var alt4Exists = NumerationHelper.IndexExists3(alternative4, graphNumeration);
-                //var alt5Exists = NumerationHelper.IndexExists3(alternative5, graphNumeration);
-                //var alt6Exists = NumerationHelper.IndexExists3(alternative6, graphNumeration);
-
-                //if (alt1Exists && alt2Exists && alt3Exists && alt4Exists && alt5Exists && alt6Exists) return null;
-
-                //int alt1Count = 0, alt2Count = 0, alt3Count = 0, alt4Count = 0, alt5Count = 0, alt6Count = 0;
-                //foreach (var v in vertices)
-                //{
-                //    if (!alt1Exists && NumerationHelper.CompareVertex(graphNumeration[v], alternative1, 3) >= 0) ++alt1Count;
-                //    if (!alt2Exists && NumerationHelper.CompareVertex(graphNumeration[v], alternative2, 3) >= 0) ++alt2Count;
-                //    if (!alt3Exists && NumerationHelper.CompareVertex(graphNumeration[v], alternative3, 3) >= 0) ++alt3Count;
-                //    if (!alt4Exists && NumerationHelper.CompareVertex(graphNumeration[v], alternative4, 3) >= 0) ++alt4Count;
-                //    if (!alt5Exists && NumerationHelper.CompareVertex(graphNumeration[v], alternative5, 3) >= 0) ++alt5Count;
-                //    if (!alt6Exists && NumerationHelper.CompareVertex(graphNumeration[v], alternative6, 3) >= 0) ++alt6Count;
-                //}
-                //if (alt1Count != vertices.Count() && alt2Count != vertices.Count() && alt3Count != vertices.Count() && alt4Count != vertices.Count() && alt5Count != vertices.Count() && alt6Count != vertices.Count()) return null;
-                //if (alt1Count == vertices.Count())
-                //{
-                //    alts.Add(alternative1);
-                //}
-                //if (alt2Count == vertices.Count())
-                //{
-                //    alts.Add(alternative2);
-                //}
-                //if (alt3Count == vertices.Count())
-                //{
-                //    alts.Add(alternative3);
-                //}
-                //if (alt4Count == vertices.Count())
-                //{
-                //    alts.Add(alternative4);
-                //}
-                //if (alt5Count == vertices.Count())
-                //{
-                //    alts.Add(alternative5);
-                //}
-                //if (alt6Count == vertices.Count())
-                //{
-                //    alts.Add(alternative6);
-                //}
-                //return alts;
             }
 
             private bool ContainsAlternative(int[] alternative)
