8a7a3686d48db898ab757122c5a38e703162f05b
Sevoster <sevoster@gmail.com>
2017-12-09 17:18:03 +0300

    Console app: save result in json format

---
+++ src/MeshRecovery_Console/JsonSerializer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MeshRecovery_Console
+{
+    class NumerationWrapper
+    {
+        public List<Vertex> VertexCoordinates { get; set; }
+
+        public class Vertex
+        {
+            public int X { get; set; }
+
+            public Vertex(int value)
+            {
+                X = value;
+            }
+        }
+
+        class Vertex2D : Vertex
+        {
+            public int Y { get; set; }
+
+            public Vertex2D(int[] numeration) : base(numeration[0])
+            {
+                Y = numeration[1];
+            }
+        }
+
+        class Vertex3D : Vertex2D
+        {
+            public int Z { get; set; }
+
+            public Vertex3D(int[] numeration) : base(numeration)
+            {
+                Z = numeration[2];
+            }
+        }
+
+        private Vertex CreateVertex(int[] coordinates)
+        {
+            switch (coordinates.Length)
+            {
+                case 1:
+                    return new Vertex(coordinates[0]);
+                case 2:
+                    return new Vertex2D(coordinates);
+                case 3:
+                    return new Vertex3D(coordinates);
+                default:
+                    throw new Exception($"Coordinates dimension is not supported: {coordinates.Length}");
+            }
+        }
+
+        public NumerationWrapper(int[][] numeration)
+        {
+            VertexCoordinates = new List<Vertex>();
+
+            for (int i = 0; i < numeration.Length; i++)
+            {
+                VertexCoordinates.Add(CreateVertex(numeration[i]));
+            }
+        }
+    }
+
+    static class JsonSerializer
+    {
+        public static string SerializeNumeration(int[][] numeration)
+        {
+            string result = null;
+            try
+            {
+                var wrapper = new NumerationWrapper(numeration);
+                var data = wrapper.VertexCoordinates
+                    .Select((value, index) => new { value, index })
+                    .ToDictionary(w => w.index, w => w.value);
+
+                result = JsonConvert.SerializeObject(data);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error during serialization: {e.Message}");
+            }
+            return result;
+        }
+    }
+}
--- src/MeshRecovery_Console/Program.cs
+++ src/MeshRecovery_Console/Program.cs
@@ -13,10 +13,9 @@ namespace MeshRecovery_Console
     {
         static void Main(string[] args)
         {
-             
-            if (args.Length == 0)
+            if (args.Length != 2)
             {
-                Console.WriteLine("No path to source file");
+                Console.WriteLine("Usage: MeshRecovery_Console.exe <path to .graph file> <path to save result>");
                 return;
             }
             long[] xadj = null;
@@ -34,7 +33,27 @@ namespace MeshRecovery_Console
             MeshRecovery.Numerate(xadj, xadj.Length, adjncy, out graphNumeration);
             timer.Stop();
             Console.WriteLine("Function Numerate finished work. Elapsed: " + timer.Elapsed);
-            //Save graphNumeration to file (discuss format)
+
+            //Save graphNumeration to file
+            string jsonString = null;
+            try
+            {
+                jsonString = JsonSerializer.SerializeNumeration(graphNumeration);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Can not serialize result: {e.Message}");
+                return;
+            }
+
+            try
+            {
+                File.WriteAllText(args[1], jsonString);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Can not save result to {args[1]}: {e.Message}");
+            }
         }
     }
 }
