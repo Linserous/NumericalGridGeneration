fb6c13a2c88d21f8ced8e5e952bd19638729c8f5
Sevoster <sevoster@gmail.com>
2017-12-10 10:26:18 +0300

    Simplified test with answers. We should only know graph is good or bad

--- src/TestProject/UnitTests/FunctionsTest.cs
+++ src/TestProject/UnitTests/FunctionsTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using MeshRecovery_Lib;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace UnitTests
 {
@@ -10,8 +11,10 @@ namespace UnitTests
     public class FunctionsTest
     {
         string PathToSources = @"tests/sources";
-        string PathToAnswers = @"tests/answers";
+        string GoodFolder = @"good";
+        string BadFolder = @"bad";
         string Fail = @"Fail";
+
         string GraphNumerationToString(int[][] graphNumeration)
         {
             if (graphNumeration == null) return Fail;
@@ -35,40 +38,48 @@ namespace UnitTests
         [TestMethod]
         public void CheckAllFilesWithBaseAlgorithm()
         {
-            string[] files = Directory.GetFiles(PathToSources);
-            Assert.AreNotEqual(files.Length, 0, "There are no test files");
+            IEnumerable<string> files = Directory.EnumerateFiles(
+                   PathToSources, "*.graph", SearchOption.AllDirectories);
+
+            Assert.AreNotEqual(files.Count(), 0, "There are no test files");
             Dictionary<Exception, string> exceptions = new Dictionary<Exception, string>();
-            for (int i = 0; i < files.Length; ++i)
+            foreach (var file in files)
             {
                 long[] xadj;
                 int[] adjncy;
                 int[][] graphNumeration;
-                string current_file_name = files[i].Remove(0, (PathToSources + "\\").Length);
-                Loader.LoadGraphFromMETISFormat(files[i], out xadj, out adjncy);
+                Loader.LoadGraphFromMETISFormat(file, out xadj, out adjncy);
                 bool valid = MeshRecovery.Validate(xadj, xadj.Length, adjncy, out int meshDimension);
                 int numerate = MeshRecovery.Numerate(xadj, xadj.Length, adjncy, out graphNumeration);
 
-                StreamReader read;
-                try
+                bool isGood = valid && numerate == 0;
+
+                if (isGood)
                 {
-                    read = new StreamReader(PathToAnswers + "/" + current_file_name.Replace(".graph", ".num"));
-                    string answer = read.ReadLine();
-                    read.Close();
-
-                    //checking expected validation and numerating results
-                    StringAssert.Contains(answer, valid.ToString() + " " + numerate.ToString());
-
-                    //checking created graph numeration 
-                    if (valid && (numerate > -1))
+                    try
                     {
                         int errCode = NumerationHelper.ValidateNumeration(xadj, adjncy, meshDimension, graphNumeration);
                         Assert.IsTrue(errCode >= 0,
                             $"ValidateNumeartion returns {errCode} for numeration {GraphNumerationToString(graphNumeration)}");
                     }
+                    catch (Exception e)
+                    {
+                        exceptions.Add(e, Path.GetFileName(file));
+                    }
                 }
-                catch (Exception e)
+
+                DirectoryInfo dirInfo = new DirectoryInfo(Path.GetDirectoryName(file));
+                if (dirInfo.Name == GoodFolder)
                 {
-                    exceptions.Add(e, current_file_name);
+                    Assert.IsTrue(isGood);
+                }
+                else if (dirInfo.Name == BadFolder)
+                {
+                    Assert.IsFalse(isGood);
+                }
+                else
+                {
+                    Assert.Fail("Should be good and bad files only");
                 }
             }
 
@@ -78,7 +89,6 @@ namespace UnitTests
                 foreach (var e in exceptions)
                     output += Environment.NewLine + "There is problem with file: " + e.Value + ". The reason: " + e.Key.Message + "|";
                 Assert.Fail(output);
-
             }
         }
     }
