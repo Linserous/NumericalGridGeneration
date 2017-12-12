5b10110d0fd9791104979f098d9be7a122b03940
Sevoster <sevoster@gmail.com>
2017-12-12 19:28:35 +0300

    Check methods results

--- src/MeshRecovery_Console/Program.cs
+++ src/MeshRecovery_Console/Program.cs
@@ -52,15 +52,27 @@ namespace MeshRecovery_Console
             Stopwatch timer = new Stopwatch();
             timer.Start();
             int meshDemention;
-            MeshRecovery.Validate(xadj, xadj.Length, adjncy, out meshDemention);
+            bool validateResult = MeshRecovery.Validate(xadj, xadj.Length, adjncy, out meshDemention);
             timer.Stop();
-            Console.WriteLine($"Function Validate finished work. Elapsed: {timer.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Function Validate finished and returned {validateResult}. Elapsed: {timer.ElapsedMilliseconds} ms");
+            if (!validateResult)
+            {
+                Console.WriteLine("Graph can not be numerated");
+                return;
+            }
+
             timer.Reset();
             timer.Start();
             int[][] graphNumeration;
-            MeshRecovery.Numerate(xadj, xadj.Length, adjncy, out graphNumeration);
+            int numerateResult = MeshRecovery.Numerate(xadj, xadj.Length, adjncy, out graphNumeration);
             timer.Stop();
-            Console.WriteLine($"Function Numerate finished work. Elapsed: {timer.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Function Numerate finished and returned {numerateResult}. Elapsed: {timer.ElapsedMilliseconds} ms");
+
+            if (numerateResult != 0)
+            {
+                Console.WriteLine("Graph can not be numerated");
+                return;
+            }
 
             //Save graphNumeration to file
             string jsonString = null;
